97a429b91861369cbe505d3512bf03eeab07fca7
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-27 16:55:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  var newkittens = kittens.splice(3, 1, 'Broom');
-  return newkittens
+  kittens.splice(3, 1, 'Broom');
+  return kittens
 }
