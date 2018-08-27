8f076d9b5c1ea067efe8c0c99c68a32b1c0de838
Dan Brimhall <dpbrimhall@hotmail.com>
2018-08-27 17:20:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,4 +24,8 @@ function appendKitten(name) {
 
 function prependKitten(name) {
   return ['Arnold', ...kittens]
+  
+function removeLastKitten() {
+  return kittens.slice(0, kittens.length - 1)
+}  
 }
